73c56ceb6ec29c354b0cc4cfc8988bca473940d9
Vandan Nagori <nagori.vandan04@gmail.com>
2026-04-09 07:58:27 +0530

    Changed env variables

--- config/datatbase.js
+++ config/datatbase.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = pool.promise();
